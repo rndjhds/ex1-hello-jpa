4d669e370c5ec942e012622b5fab227bc6c45db6
wodls1616 <wodls1616@gmail.com>
2023-08-23 16:08:46 +0900

    jpa의 영속성 컨테스트와 batch_size에 대한 기능 추가 batch_size는 쓰기 지언 SQL 저장소에 저장할 SQL의 사이즈(버퍼의 기능)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,9 +16,42 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setName("HelloJPA");
+            // 객체를 생성한 상태(비영속 상태)
+/*
+            Member member = new Member();
+            member.setId(101L);
+            member.setName("HelloJPA");
+*/
 
+            // 객체를 저장한 상태(영속 상태)
+/*            System.out.println("BEFORE");
+            em.persist(member);
+            System.out.println("AFTER");
+*/
+
+            // 영속
+/*
+            Member findMember1 = em.find(Member.class, 101L);
+            Member findMember2 = em.find(Member.class, 101L);
+            System.out.println(findMember1 == findMember2);
+*/
+            // 영속
+/*
+
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
+
+            em.persist(member1);
+            em.persist(member2);
+*/
+
+            // 영속
+            Member member = new Member(200L, "member200");
+            em.persist(member);
+
+            em.flush();
+
+            System.out.println("================================");
             tx.commit();
         }catch (Exception e){
             tx.rollback();
