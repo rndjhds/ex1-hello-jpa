97ba3798f9ce2c039d8d87820c0f56a85e31495c
wodls1616 <wodls1616@gmail.com>
2023-08-26 19:47:20 +0900

    양방향 매핑으로 설정 양방향 매핑을 했기 때문에 연관관계의 주인을 외래키를 가진 Member를 주인으로 설정 양방향 매핑이기 때문에 Member 객체를 통해서만 Member의 입력, 수정이 가능 Team으로는 Member의 입력과 수정이 불가능 오직 조회만 가능 Member에 Team을 저장할 경우 Team의 List에도 Member를 add시켜줘야 한다. em.flush(), em.clear()를 사용하지 않을경우 List에는 아무 값도 없어서 못 불러오기 떄문이다.

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -16,7 +17,6 @@ public class JpaMain {
         tx.begin();
 
         try {
-
             // 저장
             Team team = new Team();
             team.setName("TeamA");
@@ -25,23 +25,34 @@ public class JpaMain {
 
             Member member = new Member();
             member.setName("member1");
-            member.setTeam(team);
-
+            member.changeTeam(team);
             em.persist(member);
 
             em.flush();
             em.clear();
 
-            Member findMember = em.find(Member.class, member.getId());
+            Team findTeam = em.find(Team.class, team.getId());
+            List<Member> members = findTeam.getMembers();
 
-            Team findTeam = findMember.getTeam();
-            System.out.println("findTeam = " + findTeam);
+            for (Member m : members) {
+                System.out.println("m = " + m.getName());
+            }
+
+
+            /*Member findMember = em.find(Member.class, member.getId());
+            List<Member> members = findMember.getTeam().getMembers();
+            System.out.println("members = " + members);
+
+            for (Member m : members) {
+                System.out.println("m = " + m.getName());
+            }*/
 
             // findMember의 Team을 update
-            Team team1 = new Team();
+/*          Team team1 = new Team();
             team1.setName("TeamB");
             em.persist(team1);
-            findMember.setTeam(team1);
+            findMember.setTeam(team1);*/
+
 
             tx.commit();
         } catch (Exception e) {
