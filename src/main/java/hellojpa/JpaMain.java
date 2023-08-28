e2e142963cdcd65a1a5860e2db2a38fd3f5fe59a
wodls1616 <wodls1616@gmail.com>
2023-08-28 16:25:51 +0900

    em.find()는 데이터베이스를 통해서 실제 엔티티 객체를 조회하지만 em.getReference()는 데이터베이스를 바로 조회하지 않고 프록시 객체를 생성하는 역할을 한다. 프록시는 실제 엔티티를 상속하여 만들어졌기때문에 모양이 같다. 초기에는 id값(PK값)만 가지고 있지만 member.getName()과 같이 초기화를 하게 되면 실제 엔티티를 참조하게 된다. 단 영속성 컨텍스트에 엔티티가 있을 경우 프록시를 생성하지 않고 실제 엔티티를 생성한다. 같은 트랜잭션 내에서는 같은 PK값을 쓰게 될경우 ==하면 true가 나와야 하기 떄문이다. 반대인 경우에도 마찬가지이다. 준영속 상태에서 프록시를 초기화 할경우 org.hibernate.LazyInitializationException가 발생한다. 하지만 하이버네이트 버전이 5.4.1 Final로 올라가게 되면서 em.close()를 사용해도 트랜잭션이 유지되기 때문에 org.hibernate.LazyInitializationException오류가 발생하지 않는다. em.detach(), em.clear()는 원래대로 준영속 상태일 때 오류가 발생한다.

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,10 +1,8 @@
 package hellojpa;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-import java.time.LocalDateTime;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
 
 public class JpaMain {
 
@@ -17,11 +15,29 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setCreatedBy("kim");
-            member.setCreateDate(LocalDateTime.now());
-            member.setName("user1");
-            em.persist(member);
+
+            Member member1 = new Member();
+            member1.setName("member1");
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setName("member2");
+            em.persist(member2);
+
+            em.flush();
+            em.clear();
+
+            Member refMember = em.getReference(Member.class, member1.getId());
+            System.out.println("refMember = " + refMember.getClass()); // Proxy
+            //refMember.getName(); // 강제 초기화
+
+            Hibernate.initialize(refMember); // 강제 초기화
+
+
+
+            System.out.println("isLoaded = " + emf.getPersistenceUnitUtil().isLoaded(refMember));
+
+
 
             tx.commit();
         } catch (Exception e) {
@@ -31,5 +47,18 @@ public class JpaMain {
             em.close();
         }
         emf.close();
+    }
+
+    private static void printMember(Member member) {
+        System.out.println("member = " + member.getName());
+    }
+
+    private static void printMemberAndTeam(Member member) {
+        String username = member.getName();
+        System.out.println("username = " + username);
+
+        Team team = member.getTeam();
+        System.out.println("Team = " + team.getName());
+
     }
 }
