86e18a30d56d9d155c592d06f83628fbd5fe38a6
wodls1616 <wodls1616@gmail.com>
2023-08-29 12:36:31 +0900

    casecade 영속성 전이 예시 casecade는 Child와 Parent의 라이프 사이클(생성 수정 삭제...)가 유사할때 또는 또는 Child 엔티티를 Parent 하나만 소유했을때 Child를 Parent 말고도 소유할 경우 사용하지 않는것이 좋다

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,37 +17,14 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            Team teamB = new Team();
-            teamB.setName("teamB");
-            em.persist(teamB);
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
 
-            Member member1 = new Member();
-            member1.setName("member1");
-            member1.changeTeam(team);
-            em.persist(member1);
-
-            Member member2 = new Member();
-            member2.setName("member2");
-            member2.changeTeam(teamB);
-            em.persist(member2);
-
-            em.flush();
-            em.clear();
-
-            // Member m = em.find(Member.class, member1.getId());
-
-            em.createQuery("select m from Member m", Member.class).getResultList();
-
-            //System.out.println("m = " + m.getTeam().getClass());
-
-            System.out.println("==========================");
-            //System.out.println("team.name = " + m.getTeam().getName()); // 초기화
-            //System.out.println(m.getTeam().getMembers());
-            System.out.println("==========================");
+            em.persist(parent);
 
             tx.commit();
         } catch (Exception e) {
