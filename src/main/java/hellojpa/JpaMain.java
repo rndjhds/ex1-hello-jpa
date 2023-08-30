defc3c20420421ad4daafcc5b51743dc5c2fc5e3
wodls1616 <wodls1616@gmail.com>
2023-08-31 01:21:21 +0900

    값 타입 컬렉션이란 값 타입을 컬렉션에 담아서 쓰는것을 말한다. 값 타입 컬렉션은 지속적으로 추적, 변경해야 하지 않을경우에만 사용하길 권장 실무에서는 값 타입 컬렉션을 많이 사용하지 않음 값 타입 컬렉션 대신 엔티티를 만들고 일대다 관계 + 영속성 전이 + 고아객체 제거 설정을 함

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,17 +16,37 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Address address = new Address("city", "street", "zipcode");
-            Member member1 = new Member();
-            member1.setName("member1");
-            member1.setHomeAddress(address);
-            member1.setWorkPeriod(new Period());
-            em.persist(member1);
 
-            Address newAddress = new Address("newCity", address.getStreet(), address.getZipcode());
-            member1.setHomeAddress(newAddress);
+            Member member = new Member();
+            member.setName("member1");
+            member.setHomeAddress(new Address("homeCity", "street", "zipcode"));
 
-            //member1.getHomeAddress().setCity("newCity");
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("피자");
+
+            member.getAddressHistory().add(new Address("old1", "street", "zipcode"));
+            member.getAddressHistory().add(new Address("old2", "street", "zipcode"));
+
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            System.out.println("======================= START =====================");
+            Member findMember = em.find(Member.class, member.getId());
+
+            // 임베디드 타입은 side effect(부작용) 때문에 set은 막아놨다. 그래서 새로운 값으로 업데이트를 하기 위해서는 새 객체 생성 후 바꿔줘야 한다.
+            //findMember.setHomeAddress(new Address("newCity", "street", "zipcode"));
+
+            // 치킨 -> 한식
+            findMember.getFavoriteFoods().remove("치킨");
+            findMember.getFavoriteFoods().add("한식");
+
+            // 주소 값 타입 컬렉션을 변경
+            // equals(), hashCode()로 똑같은 대상을 찾음 때문에 비교하는 객체의 equals()와 hashCode()를 제대로 구현해야됨
+            findMember.getAddressHistory().remove(new Address("old1", "street", "zipcode"));
+            findMember.getAddressHistory().add(new Address("newCity", "street", "zipcode"));
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -2,14 +2,16 @@ package hellojpa;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @SequenceGenerator(name = "MEMBER_SEQ_GENERATOR",
         sequenceName = "MEMBER_SEQ",
         initialValue = 1,
         allocationSize = 50)
-public class Member  {
+public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
@@ -31,14 +33,27 @@ public class Member  {
 
     @Embedded
     @AttributeOverrides({
-            @AttributeOverride(name="city",
-                    column=@Column(name = "WORK_CITY")),
-            @AttributeOverride(name="street",
-                    column=@Column(name = "WORK_STREET")),
-            @AttributeOverride(name="zipcode",
-                    column=@Column(name = "WORK_ZIPCODE"))
+            @AttributeOverride(name = "city",
+                    column = @Column(name = "WORK_CITY")),
+            @AttributeOverride(name = "street",
+                    column = @Column(name = "WORK_STREET")),
+            @AttributeOverride(name = "zipcode",
+                    column = @Column(name = "WORK_ZIPCODE"))
     })
     private Address workAddress;
+
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns =
+        @JoinColumn(name = "MEMBER_ID")
+    )
+    @Column(name ="FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+    @ElementCollection
+    @CollectionTable(name = "ADDRESS", joinColumns =
+        @JoinColumn(name = "MEMBER_ID")
+    )
+    private List<Address> addressHistory = new ArrayList<>();
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "TEAM_ID")
@@ -118,6 +133,22 @@ public class Member  {
 
     public void setWorkAddress(Address workAddress) {
         this.workAddress = workAddress;
+    }
+
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(Set<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+    public List<Address> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setAddressHistory(List<Address> addressHistory) {
+        this.addressHistory = addressHistory;
     }
 }
 
