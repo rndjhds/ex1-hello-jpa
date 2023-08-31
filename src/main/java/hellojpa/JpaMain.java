b8260615259fc751a157f53778eab23047806577
wodls1616 <wodls1616@gmail.com>
2023-08-31 12:15:11 +0900

    Criteria는 자바 코드로 JPQL를 생성하기 떄문에 컴파일러에서 오류를 잡아줄수 있는 장점과 JPQL보다 동적 쿼리를 작성하기 쉽다. JPQL은 String 형식으로 작성하기 때문에 컴파일러에서 오류를 잡기 어렵고 동적 쿼리를 작성하기 어렵다. 하지만 쿼리를 한눈에 알 수 없어서 유지 보수 하기 어렵다는 단점이 있다. 너무 복잡하고 실용성이 없다.

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 public class JpaMain {
@@ -18,21 +21,18 @@ public class JpaMain {
 
         try {
 
+            //Criteria 사용 준비
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member> query = cb.createQuery(Member.class);
 
-            Member member1 = new Member();
-            member1.setName("kim");
-            em.persist(member1);
+            //루트 클래스 (조회를 시작할 클래스)
+            Root<Member> m = query.from(Member.class);
 
-            Member member2 = new Member();
-            member2.setName("AAkim");
-            em.persist(member2);
+            //쿼리 생성
+            CriteriaQuery<Member> cq =
+                    query.select(m).where(cb.equal(m.get("name"), "kim"));
+            List<Member> resultList = em.createQuery(cq).getResultList();
 
-            List<Member> result = em.createQuery("select m from Member m where m.name like '%kim%'", Member.class)
-                    .getResultList();
-
-            for (Member member : result) {
-                System.out.println("member.name = " + member.getName());
-            }
 
             tx.commit();
         } catch (Exception e) {
